64e205d0d4a93e5ab990babddaaf9a63722926de
John Aniebiet <janiebiet25@gmail.com>
2025-09-19 13:16:13 -0700

    2-arguments.js added

---
+++ 2-arguments.js
@@ -0,0 +1,9 @@
+const words =process.argv.slice(2);
+
+if(words.length === 0){
+    console.log('No argument')
+} else if(words.length === 1){
+    console.log('Argument found')
+} else{
+    console.log('Argument found')
+};
